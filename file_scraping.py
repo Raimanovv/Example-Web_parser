bc774aea65b9f7cd485d19d69b67940792a27fe2
Raimanovv <rustam.raimanovv@gmail.com>
2022-12-02 15:40:20 +0500

    0004 Generator yield

--- file_scraping.py
+++ file_scraping.py
@@ -2,28 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
-list_card_url = list()
-
 headers = {"User-Agent":
                "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36"}
 
-for count in range(1, 8):
-    sleep(1)
-    url = f'https://scrapingclub.com/exercise/list_basic/?page={count}'
 
-    response = requests.get(url, headers=headers)
+def get_url():
+    for count in range(1, 8):
 
-    soup = BeautifulSoup(response.text, "lxml")
+        url = f'https://scrapingclub.com/exercise/list_basic/?page={count}'
 
-    data = soup.find_all('div', class_="col-lg-4 col-md-6 mb-4")  # find
+        response = requests.get(url, headers=headers)
 
-    for i in data:
-        card_url = 'https://scrapingclub.com' + i.find('a').get('href')
-        list_card_url.append(card_url)
+        soup = BeautifulSoup(response.text, "lxml")
 
-for card_url in list_card_url:
+        data = soup.find_all('div', class_="col-lg-4 col-md-6 mb-4")  # find
+
+        for i in data:
+            card_url = 'https://scrapingclub.com' + i.find('a').get('href')
+            yield card_url
+
+
+for card_url in get_url():
     response = requests.get(card_url, headers=headers)
-
+    sleep(3)
     soup = BeautifulSoup(response.text, "lxml")
 
     data = soup.find('div', class_="card mt-4 my-4")
